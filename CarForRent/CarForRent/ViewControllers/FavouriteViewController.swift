482a4b83bf8b3a2b49b2545091d05143164360c0
Binh Dang <binh@gmail.com>
2019-05-21 10:47:06 +1000

    update favourite view

--- CarForRent/CarForRent/ViewControllers/FavouriteViewController.swift
+++ CarForRent/CarForRent/ViewControllers/FavouriteViewController.swift
@@ -8,11 +8,76 @@
 
 import UIKit
 
-class FavouriteViewController: UIViewController {
+class FavouriteViewController: UITableViewController, UISearchResultsUpdating, UISearchControllerDelegate, UISearchBarDelegate {
 
+    let tableData = ["One","Two","Three","Twenty-One"]
+    var filteredTableData = [String]()
+    var searchController = UISearchController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myLoadView()
+    }
+    
+    func myLoadView() {
+        print("viewDidLoad viewDidLoad")
+        tableView.delegate = self
+        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.autocapitalizationType = .none
+        
+        tableView.tableHeaderView = searchController.searchBar
+        
+        searchController.delegate = self
+        searchController.dimsBackgroundDuringPresentation = false // The default is true.
+        searchController.searchBar.delegate = self // Monitor when the search button is tapped.
+        
+        /** Search presents a view controller by applying normal view controller presentation semantics.
+         This means that the presentation moves up the view controller hierarchy until it finds the root
+         view controller or one that defines a presentation context.
+         */
+        
+        /** Specify that this view controller determines how the search controller is presented.
+         The search controller should be presented modally and match the physical size of this view controller.
+         */
+        definesPresentationContext = true
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if  (searchController.isActive) {
+            return filteredTableData.count
+        } else {
+            return tableData.count
+        }
+    }
+    
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        print("updateSearchResults updateSearchResults")
+        filteredTableData.removeAll(keepingCapacity: false)
+        
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (tableData as NSArray).filtered(using: searchPredicate)
+        filteredTableData = array as! [String]
+        
+        self.tableView.reloadData()
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // Configure the cell...
+        cell.detailTextLabel?.text = tableData[indexPath.row]
+        return cell
     }
 
 
