37e0688675b5b63d8ea173c476f0a5f42b3107ed
Percy <Cc@cengyuanxideMacBook-Pro.local>
2019-05-23 17:56:43 +1000

    percy

---
+++ CarForRent/CarForRent/ViewControllers/LoginDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginDetailViewController.swift
+//  CarForRent
+//
+//  Created by Yuanxi Zeng on 23/5/19.
+//  Copyright © 2019 Quang Binh Dang. All rights reserved.
+//
+
+import UIKit
+
+class LoginDetailViewController: UIViewController {
+    
+    
+}
---
+++ CarForRent/CarForRent/ViewControllers/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  CarForRent
+//
+//  Created by Yuanxi Zeng on 23/5/19.
+//  Copyright © 2019 Quang Binh Dang. All rights reserved.
+//
+
+import UIKit
+
+class registerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
